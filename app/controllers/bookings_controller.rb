2547ce8bc7057e305d5064837530c6c0600a9a9d
Emily Tregelles <etregelles@gmail.com>
2022-05-31 14:20:02 +0100

    add actions to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.item = @item
+    @booking.user = current_user
+    flash[:notice] = @booking.errors.full_messages.to_sentence unless @booking.save
+    redirect_to bookings_path(@bookings)
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path(@bookings), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit()
+  end
 end

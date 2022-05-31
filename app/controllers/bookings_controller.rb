ee08c61492d3a59786e1e35fd79d2588b03f56b5
Emily Tregelles <etregelles@gmail.com>
2022-05-31 15:57:41 +0100

    add delete functionality to bookings index view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,8 +20,9 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to bookings_path(@bookings), status: :see_other
+    redirect_to bookings_path, status: :see_other
   end
 
   private

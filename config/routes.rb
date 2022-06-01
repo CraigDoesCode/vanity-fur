ccee1d0d31ad66b4f435480375f38a685fb01a1f
Emily Tregelles <etregelles@gmail.com>
2022-06-01 12:10:10 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :items
-  resources :bookings
   # Defines the root path route ("/")
   # root "articles#index"
   resources :items do
     resources :bookings, only: [:show, :new, :create]
   end
   resources :bookings, only: [:show, :index, :destroy]
-
-  resources :user do
-    resources :items, only: [:index, :show, :new, :create]
-  end
 end

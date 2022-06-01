5482eeec65ddd9edc2365d335dda5a2f9c142627
Tilly Bishop <tillybishop@btinternet.com>
2022-06-01 15:12:08 +0100

    editing show for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,7 @@ class ItemsController < ApplicationController
 
   def create
       @item = Item.new(item_params)
+      @item.user_id = current_user.id
       if @item.save
         redirect_to item_path(@item)
       else

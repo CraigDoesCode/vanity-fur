243f8288fdfb9f8e7c5a0cae1be4b3a959a2761f
Tilly Bishop <tillybishop@btinternet.com>
2022-06-02 16:45:04 +0100

    search implemented with function to search first_name of user and by name and description of item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,16 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    if params[:query].present?
+      sql_query = <<~SQL
+      items.name ILIKE :query
+      OR items.description ILIKE :query
+      OR users.first_name ILIKE :query
+      SQL
+      @items = Item.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
       @items = Item.all
+    end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :description ],
+    associated_against: {
+      user: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end

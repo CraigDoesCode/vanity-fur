ef615c671a536dfb432add9c9d6810edf0bd8f65
Tilly Bishop <tillybishop@btinternet.com>
2022-06-02 14:39:06 +0100

    edited show page, added carousel

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,6 @@ class ItemsController < ApplicationController
 
   def create
       @item = Item.new(item_params)
-      @item.user = current_user
       if @item.save
         redirect_to item_path(@item)
       else

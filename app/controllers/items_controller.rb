ae5886c4ca0d29241d25e9b3455071598992f0aa
Tilly Bishop <tillybishop@btinternet.com>
2022-06-02 16:56:10 +0100

    made it so that the most recently added items come up first in the list of all items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-      @items = Item.all
+      @items = Item.all.reverse
   end
 
   def show

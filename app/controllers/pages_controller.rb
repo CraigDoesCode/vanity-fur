630f870c1b32be19c2dcfc536a6e0fb6c6a5d9a6
craig norford <craig.a.norford@gmail.com>
2022-06-03 14:54:48 +0100

    styling home pae

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @items = Item.all
+    @hats = Item.where(category: "hat")
+    @shoes = Item.where(category: "shoes")
+    @hoodies = Item.where(category: "hoodie")
+    @harnesses = Item.where(category: "harness")
+    @jackets = Item.where(category: "jacket")
   end
 end
